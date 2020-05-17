1e9dfa9d535e373923efd2110c0af67d55c0961f
ivashchvlad <ivashchvlad@gmail.com>
2020-05-17 20:56:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import plane from './planeicon.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={plane} className="App-logo" alt="logo" />
       </header>
+      <main className="App-main">
+        <div className="peresadki">
+
+          <h1>Количество пересадок</h1>
+          <div className="filters">
+            <p>
+              <input type="checkbox" name="option2" value="a2" />
+              <span className="checkmark"></span>
+            Все</p>
+            <p><input type="checkbox" name="option2" value="a2" />Без пересадок</p>
+            <p><input type="checkbox" name="option2" value="a2" />1 пересадка</p>
+            <p><input type="checkbox" name="option2" value="a2" />2 пересадки</p>
+            <p><input type="checkbox" name="option2" value="a2" />3 пересадки</p>
+          </div>
+        </div>
+        <div className="content">
+
+        </div>
+      </main>
     </div>
   );
 }
