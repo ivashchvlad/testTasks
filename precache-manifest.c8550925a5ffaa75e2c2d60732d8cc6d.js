e31bfd4dccd3c5cfe94563b4be8f2a1966e95fe9
ivashchvlad <ivashchvlad@gmail.com>
2020-06-08 16:22:22 +0400

    Updates

---
+++ precache-manifest.c8550925a5ffaa75e2c2d60732d8cc6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06725c662b0f85a2c5831e0d7398124",
+    "url": "/testTasks/index.html"
+  },
+  {
+    "revision": "cd6ea3bc696540e982ff",
+    "url": "/testTasks/static/css/main.64d37e9b.chunk.css"
+  },
+  {
+    "revision": "555633e1958a9bdade48",
+    "url": "/testTasks/static/js/2.4914e2f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testTasks/static/js/2.4914e2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6ea3bc696540e982ff",
+    "url": "/testTasks/static/js/main.3ca04516.chunk.js"
+  },
+  {
+    "revision": "80b953a4434c33a85845",
+    "url": "/testTasks/static/js/runtime-main.f5a43776.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/testTasks/static/media/planeicon.0c089234.svg"
+  },
+  {
+    "revision": "35aae663c7b89bf47c1cd86506e6b9cd",
+    "url": "/testTasks/static/media/s7logo.35aae663.svg"
+  }
+]);
