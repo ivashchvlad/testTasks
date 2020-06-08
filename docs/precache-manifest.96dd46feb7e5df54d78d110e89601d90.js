0b560270f0b309f424d2049242e8a21255946abc
ivashchvlad <ivashchvlad@gmail.com>
2020-06-08 15:14:15 +0400

    build for github pages

---
+++ docs/precache-manifest.96dd46feb7e5df54d78d110e89601d90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1dcf1dfca4c0b4214420fdea00c6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f965e58a95539caa6f73",
+    "url": "/static/css/main.64d37e9b.chunk.css"
+  },
+  {
+    "revision": "555633e1958a9bdade48",
+    "url": "/static/js/2.4914e2f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4914e2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f965e58a95539caa6f73",
+    "url": "/static/js/main.699ac2ca.chunk.js"
+  },
+  {
+    "revision": "234a86fa4bac938c5b27",
+    "url": "/static/js/runtime-main.9943517a.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/static/media/planeicon.0c089234.svg"
+  },
+  {
+    "revision": "35aae663c7b89bf47c1cd86506e6b9cd",
+    "url": "/static/media/s7logo.35aae663.svg"
+  }
+]);
